c910db60e37939b9bc52d616514292dbc1897384
Filipp Vasilev <ampa1r.pl@gmail.com>
2019-04-27 21:00:52 +0300

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,10 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['react']
+          }
         }
       },
       {
